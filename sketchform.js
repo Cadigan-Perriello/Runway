0c9709b192370a2924bda573a21a0730e955a803
willhbrowne <143123179+willhbrowne@users.noreply.github.com>
2024-01-05 09:30:28 -0500

    Update sketchform.js

--- sketchform.js
+++ sketchform.js
@@ -37,8 +37,9 @@ export const addSketch = async function(firstName, lastName, email, sketch){
   const databaseItems = await getDocs(collection(db, "runway"));
   try {
       databaseItems.forEach((item) => {
+        console.log(
         if (item.data().isPublic == false && item.id != "password" && item.id != "admin-password" && item.data().firstName.toLowerCase().includes(firstName.value.toLowerCase()) &&
-              item.data().lastName.toLowerCase().includes(firstName.value.toLowerCase()) &&     
+              item.data().lastName.toLowerCase().includes(lastName.value.toLowerCase()) &&     
               item.data().email.toLowerCase().includes(email.value.toLowerCase()) ){
             updateDoc(item, {
               sketch: sketch
